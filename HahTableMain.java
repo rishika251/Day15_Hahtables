bbd150b90fc1c51a824be3ed2459e1b96d5d0554
rishika251 <rishikasadineni@gmail.com>
2022-11-20 11:46:42 +0530

    Added:Ability to remove the word from message

--- HahTableMain.java
+++ HahTableMain.java
@@ -1,13 +1,15 @@
 package com.bridgelabz.hashtables;
 
+import java.util.Hashtable;
+
 public class HahTableMain {
 	public static void main(String[] args) {
-		System.out.println("wlcome in Hash Tables program");
 		System.out.println("Welcome in Hash Tables program");
 		HashTableImp1<String, Integer> hashImpl = new HashTableImp1();
-		String message = "To be or not to be";
-		String message1 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
-		String[] messageArray = message1.toLowerCase().split(" ");
+		String message = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		Hashtable<String, Integer> hashtable = new Hashtable<>();
+		message = message.replace("avoidable", "");
+		String[] messageArray = message.toLowerCase().split(" ");
 
 		for (String word : messageArray) {
 			Integer value = hashImpl.get(word);
@@ -16,7 +18,9 @@ public class HahTableMain {
 			else
 				value = value + 1;
 			hashImpl.add(word, value);
+
 		}
 		System.out.println(hashImpl);
+			System.out.println(hashImpl);
 	}
 }
